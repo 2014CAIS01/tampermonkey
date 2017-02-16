03d8052ec473b6777e03b9c7c4b7b28f78f38a3b
2014CAIS01 <zbszx040504@126.com>
2017-02-16 22:17:34 +0800

    Signed-off-by: 2014CAIS01 <zbszx040504@126.com>

---
+++ SHU Evaluate.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         SHU Evaluate
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Complete the evaluate automatically
+// @author       CSL
+// @match        http://cj.shu.edu.cn/StudentPortal/Evaluate
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var x = document.getElementsByTagName("select");
+    for(var i = 0; i < x.length; i++)
+    {
+        x[i].selectedIndex = 1;
+    }
+})();
